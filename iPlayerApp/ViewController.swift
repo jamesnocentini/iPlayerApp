9b03adf6cbf2e6d5bf2605fa9c4923cecbb437a6
James Nocentini <james.nocentini@gmail.com>
2014-10-01 15:44:11 +0100

    initial commit

---
+++ iPlayerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPlayerApp
+//
+//  Created by James Nocentini on 01/10/2014.
+//  Copyright (c) 2014 James Nocentini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
